f2451dc903e43f94a1199fac96bb0ad206ba06ea
MrChub467 <173291253+MrChub467@users.noreply.github.com>
2024-09-12 14:05:30 -0500

    Add variables and func

--- script.js
+++ script.js
@@ -1,20 +1,12 @@
-function add(num1, num2) {
-  return num1 + num2;
-}
-function subtract(num1, num2) {
-  return num1 - num2;
-}
-function multiply(num1, num2) {
-  return num1 * num2;
-}
-function divide(num1, num2) {
-  return num1 / num2;
-}
+const addition = (a, b) => a + b;
+const subtraction = (a, b) => a - b;
+const multiplication = (a, b) => a * b;
+const division = (a, b) => a / b;
 
 function operate(num1, num2, operator) {
   switch(operator) {
     case "+":
-      result = add(num1, num2)
+      result = addition(num1, num2)
       break;
     case "-":
       result = subtract(num1, num2)
@@ -28,4 +20,15 @@ function operate(num1, num2, operator) {
   }
 }
 
+function updateDisplay() {
+  displayText.textContent = arg1 + operator + arg2;
+}
+
+displayText = document.querySelector(".display-text");
 let result = "";
+
+let arg1 = "";
+let arg2 = "";
+let operator = "";
+
+updateDisplay()
