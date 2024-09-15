6277f6d109f3f6c4d955ccd33e15e11eeed42697
MrChub467 <173291253+MrChub467@users.noreply.github.com>
2024-09-15 12:57:51 -0500

    Add the final touches

--- script.js
+++ script.js
@@ -4,27 +4,34 @@ const division = (a, b) => {
 };
 
 function operate(num1, num2, op) {
-  num1 = parseFloat(num1);
-  num2 = parseFloat(num2);
-  switch(op) {
-    case "+":
-      result = num1 + num2;
-      break;
-    case "-":
-      result = num1 - num2;
-      break;
-    case "*":
-      result = num1 * num2;
-      break;
-    case "÷":
-      result = division(num1, num2)
-      break;
+  if (num1 && num2) {
+    num1 = parseFloat(num1);
+    num2 = parseFloat(num2);
+    switch(op) {
+      case "+":
+        result = num1 + num2;
+        break;
+      case "-":
+        result = num1 - num2;
+        break;
+      case "*":
+        result = num1 * num2;
+        break;
+      case "÷":
+        result = division(num1, num2)
+        break;
+    }
+    if (result.toString().length > 8) result = parseFloat(result).toExponential(4);
+    displayText.textContent = result;
+    arg1 = result;
+    arg2 = ""
+    operator = "";
   }
-  if (result.toString().length > 8) result = parseFloat(result).toExponential(4);
-  displayText.textContent = result;
-  arg1 = result;
-  arg2 = "0"
-  operator = "";
+  else {
+    displayText.textContent = "0";
+    arg1 = "";
+    arg2 = "";
+  }
 }
 
 function updateDisplay() {
@@ -93,7 +100,6 @@ function addNumber(number) {
     } else {
       arg1 += number;
     }
-    
   }
   else {
     if (arg2 === "0") {
@@ -101,7 +107,6 @@ function addNumber(number) {
     } else {
       arg2 += number;
     }
-    
   }
   updateDisplay()
 }
@@ -126,7 +131,7 @@ operatorBtns.forEach(button => {
 
 let result = "";
 let arg1 = "";
-let arg2 = "0";
+let arg2 = "";
 let operator = "";
 
 const video = document.querySelector("#video")
@@ -140,11 +145,25 @@ vidControls.addEventListener("click", () => {
     vidControls.textContent = "Play";
   }
 })
+
+function removeClass(btn) {
+  btn.classList.remove("color");
+}
+
 const KEYS = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "/", "*", "-", "+", ".", "Enter" ]
+const buttons = Array.from(document.getElementsByTagName("button"));
 document.addEventListener('keydown', function(event) {
   const key = event.key; 
+  buttons.forEach(button => {
+    if (button.textContent === key ||
+      (key === "/" && button.textContent === "÷") ||
+      (key === "Enter" && button.textContent === "=")
+    ) {
+      button.classList.add("color")
+      setTimeout(removeClass, 100, button);
+    }
+  })
   if (KEYS.includes(key)) {
-    console.log('Key pressed:', key); 
     if (!isNaN(key)) {
       addNumber(key);
     }
